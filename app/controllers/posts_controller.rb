ea8f885a51e5283c7db0632a4fb44a893ba2b33d
Stefan Page <90534062+stefpage@users.noreply.github.com>
2023-10-09 20:04:48 -0400

    completed project objectives

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+    
+    def index
+        @posts = Post.all
+        unless user_signed_in?
+            @posts.each do |post|
+            post.user = nil
+            end
+        end
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.user_id = current_user.id
+        if @post.save
+            redirect_to action: "index"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
